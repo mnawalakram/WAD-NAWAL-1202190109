da66a0f503b841acca34624d0ba02ad72d87b3bb
mnawalakram <92266818+mnawalakram@users.noreply.github.com>
2021-12-16 22:40:28 +0700

    Delete MODUL3 NAWAL directory

--- MODUL3 NAWAL/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "modul3";
-$port = "3306";
-$connect = mysqli_connect($servername,$username,$password,$dbname,$port);
-?>
